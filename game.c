9ce734cb89982e7bbf332eb671884c1539dc5158
aaaluzzi <aaaluzzi@cpp.edu>
2022-10-20 21:35:13 -0700

    Initial outline

---
+++ game.c
@@ -0,0 +1,30 @@
+//int max (10)
+
+/*function for playing
+    store num for game generated from 1-max
+    store guess
+
+    while num != guess
+        print prompt to guess
+        store input as guess
+        if guess != num
+            print hint on guess (higher/lower)
+    print win
+*/
+
+/*function for menu
+    print menu options
+    store input in choice variable
+    if choice == 1
+        play
+    else if choice == 2
+        print message to enter new max
+        store input in max variable
+    else
+        print goodbye message
+    menu()
+*/
+
+/*function main
+    menu()
+*/
